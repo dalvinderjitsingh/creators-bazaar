008424390495ca1899bd3f32aa3cf610e177445c
Dalvinderjit Singh <66400608+dalvinderjitsingh@users.noreply.github.com>
2024-05-08 23:47:21 +0800

    create home page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "@/components/Sidebar";
+
+export default function page() {
+  return (
+    <div className="mt-36">
+      {/* sidebar logic: if loged in then show other wise display none */}
+      {/* <Sidebar /> */}
+      Hey is the sidebar working??
+    </div>
+  );
+}
